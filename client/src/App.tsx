a67abe46f791d2cf290c51cdf54f0aa9861b0b0b
Vishalyadav <Shivvishal142004@gmail.com>
2026-03-23 23:06:36 +0530

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+
+import './App.css'
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-black text-white text-3xl">
+      Tailwind is Working 🚀
+    </div>
+  )
+}
+
+export default App
